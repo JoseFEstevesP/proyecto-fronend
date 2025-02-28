87af5b5a0c00dfbd2e97e070d814c46a1c9b625e
jose Esteves <joseesteves199930@gmail.com>
2025-02-27 21:07:38 -0400

    refactor: ajustar la estructura del layout en Init para mejorar la disposición de los elementos

--- src/layout/init/Init.tsx
+++ src/layout/init/Init.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from 'react';
 import Footer from '../../components/footer/Footer';
-import { Icons } from '../../components/icon/Icons';
 import { TitlePage } from '../../components/titlePage/TitlePage';
 import style from './Init.module.scss';
 
@@ -14,9 +13,6 @@ const Init = ({
 	return (
 		<TitlePage title={title}>
 			<section className={style.init}>
-				<header className={style.init__cintillo}>
-					<Icons type='fpIcons' iconName='cintillo' />
-				</header>
 				<main>{children}</main>
 				<Footer />
 			</section>
